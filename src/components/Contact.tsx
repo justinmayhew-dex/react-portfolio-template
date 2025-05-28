c7081315c14eff9aa57c082ec9f5b1bdc9f0e1a0
Justin Mayhew <justinmayhew15@yahoo.co.uk>
2025-05-28 17:27:41 +0300

    Add action to form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,53 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import '../assets/styles/Contact.scss';
 // import emailjs from '@emailjs/browser';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 
 function Contact() {
 
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
-
-  const [nameError, setNameError] = useState<boolean>(false);
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [messageError, setMessageError] = useState<boolean>(false);
-
   const form = useRef();
-
-  const sendEmail = (e: any) => {
-    e.preventDefault();
-
-    setNameError(name === '');
-    setEmailError(email === '');
-    setMessageError(message === '');
-
-    /* Uncomment below if you want to enable the emailJS */
-
-    // if (name !== '' && email !== '' && message !== '') {
-    //   var templateParams = {
-    //     name: name,
-    //     email: email,
-    //     message: message
-    //   };
-
-    //   console.log(templateParams);
-    //   emailjs.send('service_id', 'template_id', templateParams, 'api_key').then(
-    //     (response) => {
-    //       console.log('SUCCESS!', response.status, response.text);
-    //     },
-    //     (error) => {
-    //       console.log('FAILED...', error);
-    //     },
-    //   );
-    //   setName('');
-    //   setEmail('');
-    //   setMessage('');
-    // }
-  };
 
   return (
     <div id="contact">
@@ -58,6 +18,8 @@ function Contact() {
           <Box
             ref={form}
             component="form"
+            action="https://formspree.io/f/xeogylag"
+            method='POST'
             noValidate
             autoComplete="off"
             className='contact-form'
@@ -68,24 +30,14 @@ function Contact() {
                 id="outlined-required"
                 label="Your Name"
                 placeholder="What's your name?"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                error={nameError}
-                helperText={nameError ? "Please enter your name" : ""}
+                name='name'
               />
               <TextField
                 required
                 id="outlined-required"
                 label="Email / Phone"
                 placeholder="How can I reach you?"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                error={emailError}
-                helperText={emailError ? "Please enter your email or phone number" : ""}
+                name='email'
               />
             </div>
             <TextField
@@ -96,16 +48,11 @@ function Contact() {
               multiline
               rows={10}
               className="body-form"
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-              error={messageError}
-              helperText={messageError ? "Please enter the message" : ""}
+              name="message"
             />
-            <Button variant="contained" endIcon={<SendIcon />} onClick={sendEmail}>
-              Send
-            </Button>
+            <button>
+              Send <SendIcon />
+            </button>
           </Box>
         </div>
       </div>
