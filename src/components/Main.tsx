7122a34e97004b4e12f2c66702e82d9caa7c2e03
Justin Mayhew <justinmayhew15@yahoo.co.uk>
2026-06-17 10:24:07 -0700

    Upadate job title in Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,23 +7,23 @@ import Waves from "./Waves";
 function Main() {
 
   return (
-    <div style={{overflow: 'hidden'}} className="container">
-      <Waves/>
+    <div style={{ overflow: 'hidden' }} className="container">
+      <Waves />
       <div className="about-section">
         <div className="image-wrapper">
           <img src="https://res.cloudinary.com/dmmapotba/image/upload/c_crop,g_auto,h_800,w_800/kxug4ej2fulrry4z8o6s" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/justinmayhew-dex" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://linkedin.com/in/justin-mayhew-34a035317" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/justinmayhew-dex" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+            <a href="https://linkedin.com/in/justin-mayhew-34a035317" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
           </div>
           <h1>Justin Mayhew</h1>
-          <p>Web Developer</p>
+          <p>Freelance Web Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/justinmayhew-dex" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://linkedin.com/in/justin-mayhew-34a035317" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/justinmayhew-dex" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+            <a href="https://linkedin.com/in/justin-mayhew-34a035317" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
           </div>
         </div>
       </div>
